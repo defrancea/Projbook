b25eafbbeb5871825cf3f87cbda1dcbb5c9d492f
Alain Defrance <defrancea@gmail.com>
2015-09-27 12:37:16 -0700

    Split config and razor model

---
+++ Projbook.Core/Projbook/Core/Model/ConfigDocumentationPage.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace Projbook.Core.Model
+{
+    /// <summary>
+    /// Represents a documentation page.
+    /// </summary>
+    public class ConfigDocumentationPage
+    {
+        /// <summary>
+        /// The page id.
+        /// </summary>
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// The page title.
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// The page source file.
+        /// </summary>
+        public FileInfo File { get; private set; }
+
+        /// <summary>
+        /// The page index.
+        /// </summary>
+        public int Index { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ConfigDocumentationPage"/>.
+        /// </summary>
+        /// <param name="id">Initializes the required <see cref="Id"/>.</param>
+        /// <param name="title">Initializes the required <see cref="Title"/>.</param>
+        /// <param name="file">Initializes the required <see cref="File"/>.</param>
+        /// <param name="index">Initializes the required <see cref="Index"/>.</param>
+        public ConfigDocumentationPage(string id, string title, FileInfo file, int index)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.File = file;
+            this.Index = index;
+        }
+    }
+}
---
+++ Projbook.Core/Projbook/Core/Model/Page.cs
@@ -0,0 +1,50 @@
+﻿namespace Projbook.Core.Model
+{
+    /// <summary>
+    /// Represents a documentation page.
+    /// </summary>
+    public class Page
+    {
+        /// <summary>
+        /// The page id.
+        /// </summary>
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// The page title.
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// Whether the page is the home page.
+        /// </summary>
+        public bool IsHome { get; private set; }
+
+        /// <summary>
+        /// The page anchors.
+        /// </summary>
+        public Anchor[] Anchors { get; private set; }
+
+        /// <summary>
+        /// The page content.
+        /// </summary>
+        public string Content { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ConfigDocumentationPage"/>.
+        /// </summary>
+        /// <param name="id">Initializes the required <see cref="Id"/>.</param>
+        /// <param name="title">Initializes the required <see cref="Title"/>.</param>
+        /// <param name="isHome">Initializes the required <see cref="IsHome"/>.</param>
+        /// <param name="anchor">Initializes the required <see cref="Anchors"/>.</param>
+        /// <param name="content">Initializes the required <see cref="Content"/>.</param>
+        public Page(string id, string title, bool isHome, Anchor[] anchor, string content)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.IsHome = isHome;
+            this.Anchors = anchor;
+            this.Content = content;
+        }
+    }
+}
