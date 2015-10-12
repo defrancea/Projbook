8c95759575ca634eb757c4e85621205e0c729934
Alain Defrance <defrancea@gmail.com>
2015-10-11 20:48:15 -0700

    Fix build

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -22,3 +22,6 @@ git commit -m "Deploy Documentation"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+
+# Checkout master
+git checkout master
