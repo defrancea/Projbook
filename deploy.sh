baa42c615bf996e7d285bcd2e69130df97d5559d
Alain Defrance <defrancea@gmail.com>
2015-10-11 18:04:05 -0700

    Remove Symbols generation

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # exit with nonzero exit code if anything fails
 
 # deploy to nuget
-nuget pack Projbook.nuspec -OutputDirectory Projbook.Target/bin/Release/ -Symbols -Version ${TRAVIS_TAG}
+nuget pack Projbook.nuspec -OutputDirectory Projbook.Target/bin/Release/ -Version ${TRAVIS_TAG}
 nuget push Projbook.Target/bin/Release/Projbook.${TRAVIS_TAG}.nupkg ${NUGET_KEY}
 
 # inside this git repo we'll pretend to be a new user
