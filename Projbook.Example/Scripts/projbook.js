e3385de5b527881709438b50a8a66b2a24bed6d5
Alain Defrance <defrancea@gmail.com>
2015-10-22 13:29:21 -0700

    Update example with new template

--- Projbook.Example/Scripts/projbook.js
+++ Projbook.Example/Scripts/projbook.js
@@ -1,19 +1,4 @@
-﻿// Update the url when changing tab
-$('a[data-toggle="tab"]').click(function (e) {
-    document.location = this.href;
-    $('<a href="#top"></a>')[0].click();
+﻿$('body').scrollspy({
+    target: '.bs-docs-sidebar',
+    offset: 40
 });
-
-// Reset the position in the document after page loading
-var hash = document.location.hash.split('#');
-if (2 <= hash.length) {
-    // Reset the page
-    var pageId = hash[1].split('.');
-    $('a[href="#' + pageId[0] + '"]').click();
-    $('<a href="#top"></a>')[0].click();
-
-    // Reset the anchor
-    if (2 <= pageId.length) {
-        $('a[href="#' + hash[1] + '"]')[0].click();
-    }
-}
