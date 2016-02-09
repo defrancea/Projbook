e8a31d66b81736284f14614857fe996bf544a8bd
Alain Defrance <defrancea@gmail.com>
2016-02-08 21:34:10 -0800

    Fix #64 - Add glyphicons copy during Projbook install

--- resources/Install.ps1
+++ resources/Install.ps1
@@ -10,6 +10,13 @@ $contentFolder.ProjectItems.Item("projbook.css").Properties.Item("CopyToOutputDi
 $contentFolder.ProjectItems.Item("bootstrap-theme.min.css").Properties.Item("CopyToOutputDirectory").Value = 2
 $contentFolder.ProjectItems.Item("bootstrap.min.css").Properties.Item("CopyToOutputDirectory").Value = 2
 
+# Copy files in fonts folder
+$scriptsFolder.ProjectItems.Item("glyphicons-halflings-regular.eot").Properties.Item("CopyToOutputDirectory").Value = 2
+$scriptsFolder.ProjectItems.Item("glyphicons-halflings-regular.svg").Properties.Item("CopyToOutputDirectory").Value = 2
+$scriptsFolder.ProjectItems.Item("glyphicons-halflings-regular.ttf").Properties.Item("CopyToOutputDirectory").Value = 2
+$scriptsFolder.ProjectItems.Item("glyphicons-halflings-regular.woff").Properties.Item("CopyToOutputDirectory").Value = 2
+$scriptsFolder.ProjectItems.Item("glyphicons-halflings-regular.woff2").Properties.Item("CopyToOutputDirectory").Value = 2
+
 # Copy files in Scripts folder
 $scriptsFolder.ProjectItems.Item("prism.js").Properties.Item("CopyToOutputDirectory").Value = 2
 $scriptsFolder.ProjectItems.Item("projbook.js").Properties.Item("CopyToOutputDirectory").Value = 2
