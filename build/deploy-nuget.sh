18392d9e1b52cbcf9354d10d6addfb327cb39a4b
Alain Defrance <defrancea@gmail.com>
2016-10-12 22:56:03 -0700

    Fix #113 - Publish Projbook.Extension

--- build/deploy-nuget.sh
+++ build/deploy-nuget.sh
@@ -3,4 +3,6 @@ set -e # exit with nonzero exit code if anything fails
 
 # deploy to nuget
 nuget pack ./Projbook.nuspec -OutputDirectory ./ -Version ${TRAVIS_TAG}
+nuget pack ./Projbook.Extension.nuspec -OutputDirectory ./ -Version ${TRAVIS_TAG}
 nuget push ./Projbook.${TRAVIS_TAG}.nupkg ${NUGET_KEY} > /dev/null 2>&1
+nuget push ./Projbook.Extension.${TRAVIS_TAG}.nupkg ${NUGET_KEY} > /dev/null 2>&1
