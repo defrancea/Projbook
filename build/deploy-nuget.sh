64f4ce9da63d9bc924f6516dba8e387adee382be
Alain Defrance <defrancea@gmail.com>
2016-10-13 19:18:29 -0700

    Fix #130 - Expose non-packaged wkhtmltox version of Projbook

--- build/deploy-nuget.sh
+++ build/deploy-nuget.sh
@@ -4,5 +4,7 @@ set -e # exit with nonzero exit code if anything fails
 # deploy to nuget
 nuget pack ./Projbook.nuspec -OutputDirectory ./ -Version ${TRAVIS_TAG}
 nuget pack ./Projbook.Extension.nuspec -OutputDirectory ./ -Version ${TRAVIS_TAG}
+nuget pack ./Projbook.Core.nuspec -OutputDirectory ./ -Version ${TRAVIS_TAG}
 nuget push ./Projbook.${TRAVIS_TAG}.nupkg ${NUGET_KEY} > /dev/null 2>&1
 nuget push ./Projbook.Extension.${TRAVIS_TAG}.nupkg ${NUGET_KEY} > /dev/null 2>&1
+nuget push ./Projbook.Core.${TRAVIS_TAG}.nupkg ${NUGET_KEY} > /dev/null 2>&1
